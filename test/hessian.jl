e687ae4cb2dde6c3988b60f04772f615f3ff8976
Shashi Gowda <shashigowda91@gmail.com>
2020-04-15 16:04:55 -0400

    test summed up terms for Hessian

--- test/hessian.jl
+++ test/hessian.jl
@@ -2,38 +2,38 @@
 @test hesstestterms(x->x[1], [1,2]) == Term(1)
 
 # Tuple / struct
-@test hesstesttag(x->(x[1],x[2])[2], [1,2]) == Term(2)
+@test hesstestterms(x->(x[1],x[2])[2], [1,2]) == Term(2)
 
-@test hesstesttag(x->promote(x[1],convert(Float64, x[2]))[2], [1,2]) == Term(2)
+@test hesstestterms(x->promote(x[1],convert(Float64, x[2]))[2], [1,2]) == Term(2)
 
 # 1-arg linear
-@test hesstesttag(x->deg2rad(x[1]), [1,2]) == Term(1)
+@test hesstestterms(x->deg2rad(x[1]), [1,2]) == Term(1)
 
 # 1-arg nonlinear
 @test hesstestterms(x->sin(x[1]), [1,2]) == (Term(1) + Term(1) * Term(1))
 
 # 2-arg (true,true,true)
-@test hesstesttag(x->x[1]+x[2], [1,2]) == Term(1)+Term(2)
+@test hesstestterms(x->x[1]+x[2], [1,2]) == Term(1)+Term(2)
 
 # 2-arg (true,true, false)
-@test hesstesttag(x->x[1]*x[2], [1,2]) == Term(1)*Term(2)
+@test hesstestterms(x->x[1]*x[2], [1,2]) == Term(1)*Term(2)
 
 # 2-arg (true,false,false)
-@test hesstesttag(x->x[1]/x[2], [1,2]) == Term(1)*Term(2)*Term(2)
+@test hesstestterms(x->x[1]/x[2], [1,2]) == Term(1)*Term(2)*Term(2)
 
 # 2-arg (false,true,false)
-@test hesstesttag(x->x[1]\x[2], [1,2]) == Term(1)*Term(1)*Term(2)
+@test hesstestterms(x->x[1]\x[2], [1,2]) == Term(1)*Term(1)*Term(2)
 
 # 2-arg (false,false,false) 
-@test hesstesttag(x->hypot(x[1], x[2]), [1,2]) == (Term(1) + Term(2)) * (Term(1) + Term(2))
+@test hesstestterms(x->hypot(x[1], x[2]), [1,2]) == (Term(1) + Term(2)) * (Term(1) + Term(2))
 
 
 ### Array operations
 
 # copy
-@test hesstesttag(x->copy(x)[1], [1,2]) == Term(1)
-@test hesstesttag(x->x[:][1], [1,2]) == Term(1)
-@test hesstesttag(x->x[1:1][1], [1,2]) == Term(1)
+@test hesstestterms(x->copy(x)[1], [1,2]) == Term(1)
+@test hesstestterms(x->x[:][1], [1,2]) == Term(1)
+@test hesstestterms(x->x[1:1][1], [1,2]) == Term(1)
 
 # tests `iterate`
 function mysum(x)
@@ -43,19 +43,19 @@ function mysum(x)
     end
     s
 end
-@test hesstesttag(mysum, [1,2]).terms == (Term(1) + Term(2)).terms
-@test hesstesttag(mysum, [1,2.]).terms == (Term(1) + Term(2)).terms
+@test hesstestterms(mysum, [1,2]).terms == (Term(1) + Term(2)).terms
+@test hesstestterms(mysum, [1,2.]).terms == (Term(1) + Term(2)).terms
 
 using LinearAlgebra
 
 # integer dot product falls back to generic
-@test hesstesttag(x->dot(x,x), [1,2,3]) == sum(Term(i)*Term(i) for i=1:3)
+@test hesstestterms(x->dot(x,x), [1,2,3]) == sum(Term(i)*Term(i) for i=1:3)
 
 # reroutes to generic implementation (blas.jl)
-@test hesstesttag(x->dot(x,x), [1,2,3.]) == sum(Term(i)*Term(i) for i=1:3)
-@test hesstesttag(x->x'x, [1,2,3.]) == sum(Term(i)*Term(i) for i=1:3)
+@test hesstestterms(x->dot(x,x), [1,2,3.]) == sum(Term(i)*Term(i) for i=1:3)
+@test hesstestterms(x->x'x, [1,2,3.]) == sum(Term(i)*Term(i) for i=1:3)
 
 # broadcast
-@test hesstesttag(x->sum(x[1] .+ x[2]), [1,2,3.]) == Term(1) + Term(2)
-@test hesstesttag(x->sum(x .+ x), [1,2,3.]) == sum(Term(i) for i=1:3)
-@test hesstesttag(x->sum(x .* x), [1,2,3.]) == sum(Term(i)*Term(i) for i=1:3)
+@test hesstestterms(x->sum(x[1] .+ x[2]), [1,2,3.]) == Term(1) + Term(2)
+@test hesstestterms(x->sum(x .+ x), [1,2,3.]) == sum(Term(i) for i=1:3)
+@test hesstestterms(x->sum(x .* x), [1,2,3.]) == sum(Term(i)*Term(i) for i=1:3)
